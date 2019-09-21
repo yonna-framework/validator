3202dc16e0f9f8fff15b2ddb00c2ca72056489c2
hunzsig <mzyhaohaoren@qq.com>
2019-09-21 22:42:50 +0800

    array

---
+++ AbstractChecker.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Yonna\Validator;
+
+use Closure;
+
+abstract class AbstractChecker
+{
+
+    private $error = [];
+
+    /**
+     * @return string
+     */
+    public function getError(): string
+    {
+        if (empty($this->error)) {
+            return '';
+        }
+        return implode('; ', $this->error);
+    }
+
+    /**
+     * @param string $error
+     */
+    protected function error(string $error): void
+    {
+        $error && $this->error[] = $error;
+    }
+
+    /**
+     * build a checker
+     * @return $this
+     */
+    protected static function createChecker()
+    {
+        return new static();
+    }
+
+    /**
+     * @param Closure $failCallback
+     */
+    protected function callback(Closure $failCallback): void
+    {
+        if (!empty($this->error)) {
+            $failCallback(self::getError());
+        }
+    }
+
+
+}
